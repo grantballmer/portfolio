3f819bf217e0dbf6633d8df14fb6a56925255b78
Grant Ballmer <grant.a.ballmer@gmail.com>
2019-03-22 15:20:32 -0400

    optimized responsive design images

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -499,6 +499,8 @@ module.exports = removeSideNav;
 const form = document.querySelector("form");
 const honeypot = form.querySelector(".extra");
 
+// check if honeypot input field has a value, and if so prevent form submission
+
 form.addEventListener("submit", function(e) {
   if (honeypot.value !== "") {
     e.preventDefault();
--- scripts/honeypot.js
+++ scripts/honeypot.js
@@ -1,6 +1,8 @@
 const form = document.querySelector("form");
 const honeypot = form.querySelector(".extra");
 
+// check if honeypot input field has a value, and if so prevent form submission
+
 form.addEventListener("submit", function(e) {
   if (honeypot.value !== "") {
     e.preventDefault();
